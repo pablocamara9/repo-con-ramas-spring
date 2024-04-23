51882e7155ae7e1da7b6067abd533f15be90804b
Pablo <pcamaragar@gmail.com>
2024-04-23 23:02:57 +0200

    First commit

---
+++ EjemploRepoConRamas/src/main/java/com/salesianostriana/dam/ejemplorepoconramas/EjemploRepoConRamasApplication.java
@@ -0,0 +1,13 @@
+package com.salesianostriana.dam.ejemplorepoconramas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EjemploRepoConRamasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EjemploRepoConRamasApplication.class, args);
+	}
+
+}
---
+++ EjemploRepoConRamas/src/test/java/com/salesianostriana/dam/ejemplorepoconramas/EjemploRepoConRamasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.salesianostriana.dam.ejemplorepoconramas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EjemploRepoConRamasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
