416455c96d2a27922209047893fd913441ffd3bb
Pablo <pcamaragar@gmail.com>
2024-04-24 00:12:16 +0200

    Creado el paquete model y la clase Persona

---
+++ EjemploRepoConRamas/src/main/java/com/salesianostriana/dam/ejemplorepoconramas/model/Persona.java
@@ -0,0 +1,15 @@
+package com.salesianostriana.dam.ejemplorepoconramas.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Persona {
+	
+	private String nombre;
+	private String apellido;
+
+}
